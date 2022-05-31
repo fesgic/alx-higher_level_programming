8ca1c5122fa73da22ce79f71805ce4561b2653fd
fesgic <festusgichohi2@gmail.com>
2022-05-31 22:40:33 +0300

    0x01-python if_else_loops_functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,10 @@ a = "Last digit of {0:d} is {1:d} and is {2:s}"
 if (number >= 0):
     modulus = number % 10
 elif (number < 0):
-    modulus = (-number) % 10
+    modulus = -((-number) % 10)
 if (modulus > 5):
     print(a.format(number, modulus, "greater than 5"))
 elif (modulus == 0):
     print(a.format(number, modulus, "0"))
 elif (modulus < 6 and (number % 10) != 0):
-    if (number < 0):
-        print(a.format(number, -modulus, "less than 6 and not 0"))
-    else:
-        print(a.format(number, modulus, "less than 6 and not 0"))
+    print(a.format(number, modulus, "less than 6 and not 0"))
