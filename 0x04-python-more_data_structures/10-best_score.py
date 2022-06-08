f910115b9ef2692cb621da9b89225674d7ac92b7
fesgic <festusgichohi2@gmail.com>
2022-06-08 22:00:58 +0300

    0x04-python more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if (a_dictionary == None):
         return (None)
+    if (len(a_dictionary) == 0):
+        return (None)
     
     for k in a_dictionary:
         new = a_dictionary[k]
