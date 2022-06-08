37ef7567391d46af44a60e937f85f49d2df18b65
fesgic <festusgichohi2@gmail.com>
2022-06-09 00:02:08 +0300

    0x04-python more data structures

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return ([[y**2 for y in x] for x in matrix if len(matrix) != 0])
+    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """
+    Convert roman numerals to string
+    """
+    conv = {'I': 1, "X" : 10, "}
+    for i im 
