3ebfb877a10d75aa9ff0678b54b5cb63bdec54b3
fesgic <festusgichohi2@gmail.com>
2022-06-23 15:16:53 +0300

    0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square Class
+"""
+
+
+class Square:
+    """
+    Initializa a square class
+    """
+
+    def __init__(self, size):
+        """
+        Initialize siza as private instance attribute
+        """
+        self.__size = size
